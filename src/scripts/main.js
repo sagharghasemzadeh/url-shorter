02e2bec6b1b2d70a9c490911a992deab9651a9c8
BijanProgrammer <bijaneisapour@gmail.com>
2024-10-22 18:23:58 +0330

    [ADD] create link card when form is submitted

---
+++ src/scripts/main.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("section#paste form");
+const list = document.querySelector("section#links ul");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const url = formData.get("url");
+
+  addUrlToList(url);
+});
+
+function addUrlToList(url) {
+  const item = document.createElement("li");
+  item.classList.add("card");
+  item.innerHTML = url;
+
+  list.append(item);
+}
